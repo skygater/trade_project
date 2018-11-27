4c0bc103597410a82cbcb6a9812fc402d2f6fbdd
Djordje Kalezic <shirokuro.studios@gmail.com>
2018-11-27 13:10:54 +0100

    Map radius added!

--- app/src/main/java/me/craftcreative/bananaz/AddProductActivity.java
+++ app/src/main/java/me/craftcreative/bananaz/AddProductActivity.java
@@ -1,8 +1,15 @@
 package me.craftcreative.bananaz;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +17,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -17,6 +25,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.shuhart.stepview.StepView;
@@ -31,7 +41,7 @@ import me.craftcreative.bananaz.Fragments.StepTwoFragment;
 import me.craftcreative.bananaz.Tweaks.UtilsLogIn;
 import me.craftcreative.bananaz.Tweaks.UtilsProduct;
 
-public class AddProductActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class AddProductActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener {
 
     //STEPS
     StepView stepView;
@@ -42,6 +52,10 @@ public class AddProductActivity extends AppCompatActivity implements OnMapReadyC
     RelativeLayout rel_map;
     //View Init
     Button btn;
+    //Map constant
+    private static final int MY_LOCATION_REQUEST_CODE = 1;
+    private GoogleMap gMap;
+    private Location myLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +66,7 @@ public class AddProductActivity extends AppCompatActivity implements OnMapReadyC
 
         //INIT STEPS
         stepView = (StepView) findViewById(R.id.stepview);
-        List<String>name = new ArrayList<>();
+        List<String> name = new ArrayList<>();
         name.add(getString(R.string.step_one_title));
         name.add(getString(R.string.step_two_title));
         name.add(getString(R.string.step_three_title));
@@ -65,7 +79,7 @@ public class AddProductActivity extends AppCompatActivity implements OnMapReadyC
         fragmentManager = getSupportFragmentManager();
         fragmentManager
                 .beginTransaction()
-                .replace(R.id.frame_product,new StepOneFragment(), UtilsProduct.Step_One)
+                .replace(R.id.frame_product, new StepOneFragment(), UtilsProduct.Step_One)
                 .commit();
 
         // MAP test
@@ -73,17 +87,19 @@ public class AddProductActivity extends AppCompatActivity implements OnMapReadyC
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
     }
 
-    public int fragmentId =0;
-    //CHANGE FRAGMENT 1-3
-    public void fragmentChange(int anim1, int anim2){
+    public int fragmentId = 0;
 
-        switch (currentStep){
+    //CHANGE FRAGMENT 1-3
+    public void fragmentChange(int anim1, int anim2) {
+
+        switch (currentStep) {
             case 0:
                 fragmentManager
                         .beginTransaction()
-                        .setCustomAnimations(anim1,anim2)
+                        .setCustomAnimations(anim1, anim2)
                         .replace(R.id.frame_product, new StepOneFragment(),
                                 UtilsProduct.Step_One).commit();
                 fragmentId = 0;
@@ -93,7 +109,7 @@ public class AddProductActivity extends AppCompatActivity implements OnMapReadyC
                 rel_map.setVisibility(View.VISIBLE);
                 fragmentManager
                         .beginTransaction()
-                        .setCustomAnimations(anim1,anim2)
+                        .setCustomAnimations(anim1, anim2)
                         .replace(R.id.frame_product, new StepTwoFragment(),
                                 UtilsProduct.Step_Two).commit();
                 fragmentId = 1;
@@ -103,7 +119,7 @@ public class AddProductActivity extends AppCompatActivity implements OnMapReadyC
                 rel_map.setVisibility(View.GONE);
                 fragmentManager
                         .beginTransaction()
-                        .setCustomAnimations(anim1,anim2)
+                        .setCustomAnimations(anim1, anim2)
                         .replace(R.id.frame_product, new StepThreeFragment(),
                                 UtilsProduct.Step_Three).commit();
                 fragmentId = 2;
@@ -113,22 +129,23 @@ public class AddProductActivity extends AppCompatActivity implements OnMapReadyC
 
 
     // ON BACK BTN CLICK
-    public void back (View view){
+    public void back(View view) {
         if (currentStep > 0) {
             currentStep--;
         }
         stepView.done(false);
         stepView.go(currentStep, true);
         fragmentChange(R.anim.left_enter, R.anim.right_exit);
-        if(currentStep == 0){
+        if (currentStep == 0) {
             frameLayout.setVisibility(View.VISIBLE);
             rel_map.setVisibility(View.GONE);
             btn.setVisibility(View.GONE);
         }
 
     }
+
     // ON NEXT BTN CLICK
-    public void next (View view){
+    public void next(View view) {
         if (fragmentId == 0) {
             StepOneFragment fragment = (StepOneFragment) fragmentManager.findFragmentByTag(UtilsProduct.Step_One);
             if (!fragment.checkEditText()) {
@@ -136,9 +153,9 @@ public class AddProductActivity extends AppCompatActivity implements OnMapReadyC
                 return;
             }
         }
-        if (currentStep>=2){
+        if (currentStep >= 2) {
             Toast.makeText(this, "Done!", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this,MainActivity.class);
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             return;
         }
@@ -151,28 +168,86 @@ public class AddProductActivity extends AppCompatActivity implements OnMapReadyC
             currentStep++;
         }
         btn.setVisibility(View.VISIBLE);
-        fragmentChange(R.anim.right_enter,R.anim.left_exit);
+        fragmentChange(R.anim.right_enter, R.anim.left_exit);
 
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        gMap.setMyLocationEnabled(true);
+        ImageButton img  = (ImageButton) findViewById(R.id.locateMe);
+
+
+        gMap.setOnMyLocationButtonClickListener(this);
+        gMap.setOnMyLocationClickListener(this);
+
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        myLocation = locationManager.getLastKnownLocation(locationManager
+                .getBestProvider(criteria, false));
+        LatLng sydney = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+        Circle radius = gMap.addCircle(new CircleOptions().center(sydney).radius(1000)
+                .strokeColor(getResources().getColor(R.color.colorPrimaryDark))
+                .fillColor(getResources().getColor(R.color.radiusIn))
+        );
+        gMap.getMaxZoomLevel();
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+
+            if (permissions.length == 1 && permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                gMap.setMyLocationEnabled(true);
+
+            }
+
+        }else{
+
+        }
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker( new MarkerOptions().position(sydney))
-                .setTitle("Marker in Sydney");
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public void onMyLocationClick(@NonNull Location location) {
+        myLocation = location;
     }
 
+    @Override
+    public boolean onMyLocationButtonClick() {
 
-//    private static final int RC_CAMERA = 3000;
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//            if (requestCode == RC_CAMERA) {
-//                if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    //captureImage();
-//                }
-//            }
-//            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//
-//    }
+        return false;
+    }
+
+    public void locateMe (View view){
+
+    }
 }
